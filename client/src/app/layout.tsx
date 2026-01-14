a4c1f8ebac9d6f010bb9e4caf8778c05a1510151
Shasbin  AS <shasbinas1@gmail.com>
2026-01-14 15:21:24 +0530

    Fix: correct the error

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import DashboardWrapper from "./dashboardWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
